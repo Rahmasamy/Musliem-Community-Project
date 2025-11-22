9969b0838c978a2e2821051e3fc24c35ce1a12a3
Rahmasamy <rahmasamy949@gmail.com>
2025-11-22 18:58:31 +0200

    change base url

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,6 +1,7 @@
 import axios from "axios";
+const Base_Url = import.meta.env.Base_Url  || "http://localhost:5000/api"
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: Base_Url,
     withCredentials: true,
 });
 export default axiosInstance;
